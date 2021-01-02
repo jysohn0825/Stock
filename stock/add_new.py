a077136ba6560a4635c5237e7d89a8f1fe5578c8
RB_ <jysohn0825@naver.com>
2021-01-02 16:00:47 +0900

    Add files via upload

--- stock/add_new.py
+++ stock/add_new.py
@@ -1,10 +1,18 @@
+from eunjeon import Mecab
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 import FinanceDataReader as fdr
 from datetime import datetime, timedelta
-TIME = datetime.today().strftime("%Y-%m-%d")
+TODAY = datetime.today().strftime("%Y-%m-%d")
+YESTERDAY = (datetime.strptime(TODAY, '%Y-%m-%d') + timedelta(days=-1)).strftime("%Y-%m-%d")
 LAST_TIME = (datetime.today() - timedelta(180)).strftime("%Y-%m-%d")
 import csv
 
+dicList = pd.read_csv("../crawling/dictionary.csv", usecols=[1,2], encoding="CP949")
+dic = {}
+for d in dicList.values:
+    dic[d[0]] = d[1]
 
 def stockData():
     sample = pd.read_csv("./Total.csv", usecols=[0,1], encoding='cp949')
@@ -13,23 +21,95 @@ def stockData():
         code = str(i[1])
         code = "0" * (6 - len(code)) + code
         if name != '게임테마' and name != '겨울테마' and name != '자율주행차테마' and name != '항공여행테마' and name != '인공지능테마':
-            df = fdr.DataReader(code, LAST_TIME, TIME)
+            df = fdr.DataReader(code, LAST_TIME, TODAY)
             df['5Days'] = df['Close'].rolling(5).mean()
             df['10Days'] = df['Close'].rolling(10).mean()
             df['20Days'] = df['Close'].rolling(20).mean()
             df['60Days'] = df['Close'].rolling(60).mean()
             df['120Days'] = df['Close'].rolling(120).mean()
-            df['종목코드'] = code
-            df['회사명'] = name
+            df['emotion'] = 0
+            df['count'] = 0
+            df = df.reset_index()
+            flag = False
+            page = 1
+            find_emo = {}
+            while True:
+                request = requests.get(f"http://www.paxnet.co.kr/news/{code}/stock?currentPageNo={page}")
+                soup = BeautifulSoup(request.text, "lxml")
+                ul = soup.find("div", {"id": "contents"})
+                div1 = ul.find("div", {"class": "board-thumbnail"})
+                if div1 != None:
+                    div2 = div1.find("ul", {"class": "thumb-list"})
+                    li = div2.find_all("li")
+                    for i in li:
+                        date = i.find("dl").find("dd", {"class": "date"}).find_all("span")[1].text.split(" ")
+                        if date[0] >= YESTERDAY:
+                            news = i.find("dl").find("dd", {"class": "date"}).find_all("span")[0].text
+                            if news != "인포스탁" and (date[1] < "09:00" or date[1] > "15:30"):
+                                link = "http://www.paxnet.co.kr" + i.find("dl").find("dt").find("a")["href"]
+                                subRequest = requests.get(link)
+                                subSoup = BeautifulSoup(subRequest.text, "lxml")
+                                div = subSoup.find("div", {"id": "contents"}).find("div", {"class": "cont-area"}).find(
+                                    "div", {
+                                        "class": "report-view"}).find("div", {"class": "report-view-cont"})
+                                p = div.find_all("p")
+                                content = div.find("div", {"id": "span_article_content"})
+                                textSum = []
+                                for cont in p:
+                                    textSum.append(cont.text)
+                                textTemp = "".join(textSum).join(content.text)
+                                dateT = datetime.strptime(date[0], "%Y.%m.%d")
+
+                                if date[1] > "15:30":
+                                    dateT += timedelta(days=1)
+                                    tempDateT = str(dateT).split(" ")
+                                    emoDate = tempDateT[0].replace(".", "-")
+                                    if emoDate == YESTERDAY:
+                                        find_emo[emoDate] = find_emo.get(emoDate, "") + textTemp
+                                        df.loc[df["Date"] == emoDate, "count"] += 1
+                                elif date[1] < "09:00":
+                                    emoDate = date[0].replace(".", "-")
+                                    if emoDate == TODAY:
+                                        find_emo[emoDate] = find_emo.get(emoDate, "") + textTemp
+                                        df.loc[df["Date"] == emoDate, "count"] += 1
+                        else:
+                            flag = True
+                            break
+                    page += 1
+                    if flag:
+                        break
+                else:
+                    break
+            for k, v in find_emo.items():
+                count, score = 0, 0
+                m = Mecab()
+                list = m.morphs(v)
+                for word in list:
+                    if word in dic.keys():
+                        if dic[word] == 1:
+                            score += 1
+                        count += 1
+                if count != 0:
+                    for sc in df.values:
+                        if str(sc[0]) == k:
+                            temp = int((score / count) * 100)
+                            emotion = 0
+                            if temp > 58:
+                                emotion = 1
+                            elif temp <= 46:
+                                emotion = -1
+                            df.loc[df["Date"] == k, "emotion"] = emotion
+
             df = df.dropna()
             df.reset_index(drop=False, inplace=True)
             df = df.iloc[-1]
-            df1 = [str(df[0])[:10]]
-            for j in range(1,len(df)) :
+            df1 = [str(df[1])[:10]]
+            for j in range(2,len(df)) :
                 df1.append(df[j])
-            fs = open(f'./작업/{name}.csv', 'a', newline='', encoding='utf-8-sig')
+            fs = open(f'./temp/{name}.csv', 'a', newline='', encoding='UTF-8-sig')
             wr = csv.writer(fs)
             wr.writerow(df1)
             fs.close()
+        print(name,"Done")
 
 stockData()
--- stock/avg_new.py
+++ stock/avg_new.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import csv
+import math
+import numpy as np
 
 def themeAverage() :
-    themelist = ['게임', '겨울', '인공지능', '자율주행차', '항공여행']
+    themelist = ['게임']#, '겨울', '인공지능', '자율주행차', '항공여행']
     for k in themelist:
-        sample = pd.read_csv(f'./작업/{k}.csv', usecols=[0, 1], encoding='cp949')
+        sample = pd.read_csv(f'./temp/{k}.csv', usecols=[0, 1], encoding='UTF-8')
         namelist = []
-        for m in sample.values:
+        for m in sample.values[:-1]:
             name = m[0]
-            namelist.append(pd.read_csv(f'./작업/{name}.csv'))
-        df = pd.DataFrame(
-            columns=['Date', 'Open', 'High', 'Low', 'Close', '5Days', '10Days', '20Days', '60Days', '120Days', '종목코드',
-                     '테마명'])
+            temp = pd.read_csv(f'./temp/{name}.csv', encoding="CP949")
+            namelist.append(temp)
+        df_columns = ['Date', 'Open', 'High', 'Low', 'Close', '5Days', '10Days', '20Days', '60Days', '120Days', 'emotion', 'count']
+        df = pd.DataFrame(columns=df_columns)
         df = namelist[0]
-        del df['회사명']
         for i in range(0, len(namelist)):
             df['Date'] = namelist[i]['Date']
             df['Open'] += namelist[i]['Open']
@@ -27,17 +28,8 @@ def themeAverage() :
             df['20Days'] += namelist[i]['20Days']
             df['60Days'] += namelist[i]['60Days']
             df['120Days'] += namelist[i]['120Days']
-            if k == '게임':
-                df['종목코드'] = '123456'
-            elif k == '겨울':
-                df['종목코드'] = '234567'
-            elif k == '항공여행':
-                df['종목코드'] = '345678'
-            elif k == '자율주행차':
-                df['종목코드'] = '456789'
-            else :
-                df['종목코드'] = '567890'
-            df['테마명'] = k
+            df['emotion'] += namelist[i]['emotion']
+            df['count'] += namelist[i]['count']
         df['Open'] //= len(namelist)
         df['High'] //= len(namelist)
         df['Low'] //= len(namelist)
@@ -49,11 +41,24 @@ def themeAverage() :
         df['20Days'] /= len(namelist)
         df['60Days'] /= len(namelist)
         df['120Days'] /= len(namelist)
+        df['emotion'] /= len(namelist)
+        df['count'] //= len(namelist)
+        count = 0
+        for i in df['emotion'].values:
+            if i > 0:
+                df.loc[count, 'emotion'] = 1
+            elif i < 0:
+                df.loc[count, 'emotion'] = -1
+            else:
+                df.loc[count, 'emotion'] = 0
+            count += 1
         df = df.iloc[-1]
-        df1 = [str(df[0])[:10]]
-        for j in range(1, len(df)):
+        df1 = []
+        for j in df_columns:
             df1.append(df[j])
-        fs = open(f'./작업/{k}테마.csv', 'a', newline='', encoding='utf-8-sig')
+        fs = open(f'./{k}테마.csv', 'a', newline='', encoding='utf-8-sig')
         wr = csv.writer(fs)
         wr.writerow(df1)
         fs.close()
+
+themeAverage()
